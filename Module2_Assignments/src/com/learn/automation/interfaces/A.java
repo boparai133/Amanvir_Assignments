ad7a54ab802f9b173dc93d960a24e0863bc77548
boparai133 <boparai.aman133@gmail.com>
2024-12-18 18:09:45 -0500

    Assignment4 (#5)
    * Assignment 4
    
    Assignment 4
    
    * Update .gitignore

---
+++ Module2_Assignments/src/com/learn/automation/interfaces/A.java
@@ -0,0 +1,8 @@
+package com.learn.automation.interfaces;
+
+public interface A {
+	public int a = 10;
+	public int b = 20;
+	void sum();
+	void add();
+}
---
+++ Module2_Assignments/src/com/learn/automation/interfaces/B.java
@@ -0,0 +1,7 @@
+package com.learn.automation.interfaces;
+
+public interface B {
+	public int x=20;
+	public int y=30;
+	void mul();
+}
