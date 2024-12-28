3ef33fec45efd712bd3f30875ff2f40d52f0e1b3
boparai133 <boparai.aman133@gmail.com>
2024-12-28 17:42:50 -0500

    Assignment5
    Assignment 5

---
+++ Module2_Assignments/src/com/learn/automation/model/AgeNotZeroException.java
@@ -0,0 +1,29 @@
+package com.learn.automation.model;
+
+public class AgeNotZeroException extends Exception {
+
+	public AgeNotZeroException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AgeNotZeroException(String message) {
+		super(message);		
+		// TODO Auto-generated constructor stub
+	}
+
+	public AgeNotZeroException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AgeNotZeroException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AgeNotZeroException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
